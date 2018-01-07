9a954d92100690ff102a5eef727d47b192a4274b
Anastasia Lucas <ani@theta.local>
2018-01-07 17:48:48 -0500

    formatting

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 const Projects = (props) =>
-  <div>
-    <h1>Projects Page</h1>
-    <p>
-    {props.tagline}{props.title}{props.background}{props.role}
-    </p>
-  </div>;
+<div className="props-container" >
+
+     <div className="props-body">
+       <div className="props-intro">
+         <h2>{props.title}</h2>
+         { props.tagline }
+         <ul className="tech-icon-container">
+
+         </ul>
+       </div>
+       <div className="desktop-screenshot-container no-mobile">
+         { <img src={props.desktop_image}/> }
+       </div> 
+       <ul className="props-links">
+         <a href={props.props_url} target="_blank">
+           <li>View props
+           <i className="fa fa-external-link-square"></i></li>
+         </a>
+         {props.github_url ?
+           <a href={props.github_url} target="_blank">
+             <li>Github
+             <i className="fa fa-external-link-square"></i></li>
+           </a>
+         : null }
+       </ul>
+       <div className="props-details">
+         {props.role}
+       </div>
+     </div>
+   </div>;
 
 export default Projects;
